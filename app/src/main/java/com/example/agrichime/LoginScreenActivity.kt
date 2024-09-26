58c45f31a052e60e04b48a968892aa7e1c4e7686
Jubibani <strawberryloli3@gmail.com>
2024-09-27 03:06:35 +0800

    connecting existing screen activities

--- app/src/main/java/com/example/agrichime/LoginScreenActivity.kt
+++ app/src/main/java/com/example/agrichime/LoginScreenActivity.kt
@@ -9,24 +9,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-class MainActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.login_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginMain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-        val socialMediaButton = findViewById<Button>(R.id.button2)
-
-        socialMediaButton.setOnClickListener {
-            val intent = Intent(this, SocialMediaScreen::class.java)
+        val forgotPassword = findViewById<TextView>(R.id.forgotPassword)
+        forgotPassword.setOnClickListener {TODO ("connect to forgot password page") }
+        val signUpLink = findViewById<TextView>(R.id.signUpLink)
+        signUpLink.setOnClickListener { TODO("connect to sign up page") }
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
     }
 }
