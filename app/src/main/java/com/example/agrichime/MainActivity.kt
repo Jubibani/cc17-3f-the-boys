add24ce3b106ec3250ad7d89a08218eb71342799
Jubibani <strawberryloli3@gmail.com>
2024-09-28 13:56:01 +0800

    modify Home Screen

--- app/src/main/java/com/example/agrichime/MainActivity.kt
+++ app/src/main/java/com/example/agrichime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.agrichime
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
