dffbf7f814c273bdedba2858827c9f91bfd0ef5d
ChrisPaoloSab-it <cpaocastillos@gmail.com>
2024-10-06 17:12:29 +0800

    resolving conflicts

--- app/src/main/java/com/example/agrichime/MainActivity.kt
+++ app/src/main/java/com/example/agrichime/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.agrichime
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -20,21 +21,31 @@ class LogInScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val usernameTemporary = "agrichimetheboys"
-        val passwordTemporary = "sagulaymaybuhay"
+
+        //val errorMessage = "Invalid username or password"
         val forgotPassword = findViewById<TextView>(R.id.forgotPassword)
-        forgotPassword.setOnClickListener {TODO ("connect to forgot password page") }
+        forgotPassword.setOnClickListener { TODO ("connect to forgot password page") }
+
         val signUpLink = findViewById<TextView>(R.id.signUpLink)
         signUpLink.setOnClickListener { TODO("connect to sign up page") }
+
+        //login button, username, and password
         val loginButton = findViewById<Button>(R.id.loginButton)
         loginButton.setOnClickListener {
             val enteredUsername = findViewById<EditText>(R.id.loginEmail).text.toString()
             val enteredPassword = findViewById<EditText>(R.id.loginPassword).text.toString()
+            val usernameTemporary = "agrichimetheboys"
+            val passwordTemporary = "sagulaymaybuhay"
 
             if (enteredUsername == usernameTemporary && enteredPassword == passwordTemporary) {
 
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
+            }
+            else {
+                //display error message
+                val errorMessage = findViewById<TextView>(R.id.errorMessage)
+                errorMessage.visibility = View.VISIBLE
             }
         }
 
